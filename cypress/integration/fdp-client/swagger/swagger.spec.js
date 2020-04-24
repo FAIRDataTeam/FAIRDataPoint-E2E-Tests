723d0625efb3d1365189a411651dd1cde5ed139e
Jan Slifka <jan.slifka@gmail.com>
2020-04-24 16:24:54 +0200

    Add basic swagger test

---
+++ cypress/integration/fdp-client/swagger/swagger.spec.js
@@ -0,0 +1,16 @@
+describe('Swagger', () => {
+    it('works', () => {
+        cy.visitClient('/swagger-ui.html')
+        cy.get('h2.title').contains('FAIR Data Point API')
+
+        cy.get('a').contains('config-controller').click()
+        cy.get('#operations-config-controller-getDashboardUsingGET').click()
+        cy.get('.btn').contains('Try it out').click()
+        cy.get('.btn').contains('Execute').click()
+        cy.get('h4')
+            .contains('Server response')
+            .parent()
+            .get('table .response-col_status')
+            .contains('200')
+    })
+})
