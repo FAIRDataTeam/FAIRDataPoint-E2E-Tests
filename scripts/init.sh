e8f93ac0fd234dc633dea47d8784c2303fdd8dae
dennisvang <29799340+dennisvang@users.noreply.github.com>
2024-11-05 12:05:55 +0100

    clean up scripts/init.sh
    - add comments
    - remove -r flag (recursive not needed when copying file)
    - verbose option --in-place for clarity

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
 
-# Init docker-compose file
+# Create a docker-compose file based on template
+
+# Get Docker image names from environment, with fallback to develop
 SERVER_IMAGE="${SERVER_IMAGE:-fairdata/fairdatapoint:develop}"
 CLIENT_IMAGE="${CLIENT_IMAGE:-fairdata/fairdatapoint-client:develop}"
 
+# Path to Docker Compose file (to be created)
 DOCKER_COMPOSE_FILE=fdp/docker-compose.yml
 
-cp -r fdp/docker-compose.template.yml $DOCKER_COMPOSE_FILE
-sed -i.bak "s#{SERVER_IMAGE}#$SERVER_IMAGE#" $DOCKER_COMPOSE_FILE && rm $DOCKER_COMPOSE_FILE".bak"
-sed -i.bak "s#{CLIENT_IMAGE}#$CLIENT_IMAGE#" $DOCKER_COMPOSE_FILE && rm $DOCKER_COMPOSE_FILE".bak"
+# Copy template into new Docker Compose file
+cp fdp/docker-compose.template.yml $DOCKER_COMPOSE_FILE
 
+# Replace Docker image names in newly created Docker Compose file
+# (backup file as .bak, remove backup after successful edit)
+# (uses `#` as `sed` delimiter, instead of default `/`, because the search and replace strings contain `/`)
+sed --in-place=.bak "s#{SERVER_IMAGE}#$SERVER_IMAGE#" $DOCKER_COMPOSE_FILE && rm $DOCKER_COMPOSE_FILE".bak"
+sed --in-place=.bak "s#{CLIENT_IMAGE}#$CLIENT_IMAGE#" $DOCKER_COMPOSE_FILE && rm $DOCKER_COMPOSE_FILE".bak"
+
+# Show result
 echo "Initialized docker-compose.yml:"
 cat $DOCKER_COMPOSE_FILE
