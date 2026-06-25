5258c83f526ac593cbc491ae0a0557c641fe346a
dennisvang <29799340+dennisvang@users.noreply.github.com>
2026-06-25 15:13:00 +0200

    fix swagger-ui expectations

--- cypress/e2e/swagger/swagger.spec.js
+++ cypress/e2e/swagger/swagger.spec.js
@@ -1,8 +1,9 @@
 describe('Swagger', () => {
     it('works', () => {
         cy.visit('/swagger-ui.html')
-        cy.get('h2.title').contains('FAIR Data Point API')
+        cy.get('h1.title').contains('FAIR Data Point API')
 
+        cy.get('a').contains('Client').click()
         cy.get('a').contains('configs').click()
         cy.get('.btn').contains('Try it out').click()
         cy.get('.btn').contains('Execute').click()
